c0f2216409ddcbd764f21f0b7cccfae5ea302f95
bysir-zl <a1019654929>
2019-02-23 23:46:10 +0800

    update comment

--- 0229_majority-element-ii_test.go
+++ 0229_majority-element-ii_test.go
@@ -8,28 +8,40 @@ import (
 // 求众数 II
 // 难度 中等
 // 多数投票法
+// 相关题目: 169
 //
-// desc
+// 给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+//
+//说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+//
+//示例 1:
+//
+//输入: [3,2,3]
+//输出: [3]
+//示例 2:
+//
+//输入: [1,1,1,3,3,2,2,2]
+//输出: [1,2]
 
 func TestL229(t *testing.T) {
-	a := majorityElement([]int{2, 2, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9})
+	a := majorityElement2([]int{2, 2, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9})
 	if !compareIntSlice(a, []int{3, 9}) {
 		t.Fail()
 	}
-	a = majorityElement([]int{1, 2})
+	a = majorityElement2([]int{1, 2})
 	if !compareIntSlice(a, []int{1, 2}) {
 		t.Fail()
 	}
-	//a = majorityElement([]int{1, 1, 2})
-	//if !compareIntSlice(a, []int{1}) {
-	//    t.Fail()
-	//}
+	a = majorityElement2([]int{1, 1, 2})
+	if !compareIntSlice(a, []int{1}) {
+		t.Fail()
+	}
 }
 
 // 注意这个题返回的结果最多只有两个元素
 // 多数投票法
 // https://blog.csdn.net/kimixuchen/article/details/52787307
-func majorityElement(nums []int) []int {
+func majorityElement2(nums []int) []int {
 	var r []int
 	if len(nums) == 0 {
 		return r
